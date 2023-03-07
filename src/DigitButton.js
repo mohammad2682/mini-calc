9011935f6197d957f58fc32c6cd295093264ce11
mohammad2682 <mohammad.akradi96@gmail.com>
2023-03-07 16:16:10 +0330

    simple calculator

---
+++ src/DigitButton.js
@@ -0,0 +1,11 @@
+import { ACTIONS } from "./App"
+
+export default function DigitButton({className, dispatch, digit}) {
+    return (
+        <button 
+        className={className}
+        onClick={() => dispatch({type:ACTIONS.ADD_DIGIT, payload: {digit}})}>
+            {digit}
+        </button>
+    )
+}
